448ac5b17f55d44f3d599fb7f7bbedcccfdd75db
sa8a <jpkrllll@yahoo.co.jp>
2022-12-25 23:57:53 +0900

    HomeウィジェットをStatefulWidgetに変更 #3

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
