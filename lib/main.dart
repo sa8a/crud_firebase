c13dc168509dc6a81043d4d5be54934922616ab3
sa8a <jpkrllll@yahoo.co.jp>
2022-12-26 01:22:56 +0900

    add: Createページのルート追加 #6

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:crud_firebase/pages/home_page.dart';
+import 'package:crud_firebase/pages/add_name_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const Home(),
+        '/add': (context) => const AddNamePage(),
       },
     );
   }
